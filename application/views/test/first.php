86076a48aba9d09a1fb10290cf7fea553d0f6d77
Nikita Volynkin <niknorff@gmail.com>
2017-01-16 13:40:31 +0200

    Test controller is ready

---
+++ application/views/test/first.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>First method</h1>
+	<p>This is the first example about view.</p>
+	<?php echo "Firstname = '.$fname'"; ?>
+</body>
+</html>
