f6e74d722510b05f0354afb4c4f70fda19b8204b
Nikita Volynkin <niknorff@gmail.com>
2017-01-16 14:35:29 +0200

    Customers controller created

---
+++ application/views/customer/show_customers.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Customers</h1>
+	<?php
+	  print_r($customers);
+	?>
+</body>
+</html>
